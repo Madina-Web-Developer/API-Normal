c8edb70d4bcb5cc69240132561935d058e20f58a
Madina-Web-Developer <developermadina761@gmail.com>
2025-02-08 13:24:29 +0600

    vhdsagfyh

---
+++ src/Cpmponents/Api.jsx
@@ -0,0 +1,39 @@
+
+import './Api.css'
+
+const Api = () => {
+
+   
+
+  return (
+    <>
+
+     <p>Application Programming Interface Practice</p>
+
+
+    <div className="main_card">
+
+       
+
+                    <div className="single_card">
+
+                        
+                        <div className="img">
+
+                    <img src=''  alt="n" />
+
+                        </div>
+                   <div className="text">
+                   <h2>$</h2>
+                    <h1></h1>
+                    <h3></h3>
+                   </div>
+                    </div>
+
+    </div>
+    
+    </>
+  )
+}
+
+export default Api
